bb13cb1327fa6ba67eeef903acc54d322fbbc943
Erico-Muller <ericomuller0@gmail.com>
2024-06-27 10:47:52 -0300

    add corretion in robot.ino

--- robot.ino
+++ robot.ino
@@ -5,18 +5,18 @@
 #include "./env.h"
 #include "./modules/modules.h"
 
-MC motion;
+H_Bridge_MC motion;
 Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN);
 
 void setup() {
   Serial.begin(9600);
 
-  motion.init(motor1Pins, motor2Pins);
+  motion.start(motor1Pins, motor2Pins);
   // caso os motores estejam invertidos, invertam a ordem dos parâmetros
 }
 
 void loop() {
-  motion.speed(80);
+  motion.set_speed(80);
   motion.forward();
 
   int distance = ultrasonic.read();
@@ -29,19 +29,19 @@ void loop() {
     motion.stop();
     delay(SMALL_DELAY);
 
-    motion.to_left();
+    motion.rotate_to_left();
     motion.forward();
     delay(SMALL_DELAY);
 
-    motion.to_right();
+    motion.rotate_to_right();
     motion.forward();
     delay(SMALL_DELAY);
 
-    motion.to_right();
+    motion.rotate_to_right();
     motion.forward();
     delay(SMALL_DELAY);
 
-    motion.to_left();
+    motion.rotate_to_left();
     motion.forward();
     delay(SMALL_DELAY);
   }
