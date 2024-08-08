885ee13bba7fdcf511f049305b5e98dedaa8e9d5
Erico-Muller <ericomuller0@gmail.com>
2024-08-08 08:20:55 -0300

    improve test a little

--- env.h
+++ env.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define DEFAULT_SPEED 41
+
 #define SMALL_DELAY 1000
 #define LARGE_DELAY 2500
 
@@ -12,3 +14,6 @@ const int motor1Pins[] = {5, 6};
 const int motor2Pins[] = {10, 11};
 const int motor3Pins[] = {4, 3};
 const int motor4Pins[] = {9, 8};
+
+#define irQty 5
+const int irPins[5] = {42, 40, 38, 36, 34};
--- robot.ino
+++ robot.ino
@@ -9,8 +9,7 @@ H_Bridge_MC motion1;
 H_Bridge_MC motion2;
 Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN);
 
-const int pins[5] = {42, 40, 38, 36, 34};
-int d[5] = {0, 0, 0, 0, 0};
+int ir[irQty] = {0, 0, 0, 0, 0};
 
 void setup() {
   Serial.begin(9600);
@@ -19,68 +18,63 @@ void setup() {
   motion2.start(motor3Pins, motor4Pins);
   // caso os motores estejam invertidos, invertam a ordem dos parâmetros
 
-  for (short i = 0; i < 5; i++) {
-    pinMode(pins[i], INPUT);
+  for (short i = 0; i < irQty; i++) {
+    pinMode(irPins[i], INPUT);
   }
 }
 
 void loop() {
-  for (short i = 0; i < 5; i++) {
-    d[i] = digitalRead(pins[i]);
+  for (short i = 0; i < irQty; i++) {
+    d[i] = digitalRead(irPins[i]);
   }
 
-  motion1.set_speed(41);
-  motion2.set_speed(41, true);
-  // motion1.forward();
-  // motion2.forward();
+  int distance = ultrasonic.read();
 
-  if (!d[2]) {
-    motion1.forward();
-    motion2.forward();
-  } else if (!d[1]) {
-    motion1.right();
-    motion2.right();
-  } else if (!d[3]) {
-    motion1.left();
-    motion2.left();
-  } else {
-    motion1.stop();
-    motion2.stop();
-  }
-
-  /*int distance = ultrasonic.read();
-
-  Serial.print("Distancia: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  motion1.set_speed(DEFAULT_SPEED);
+  motion2.set_speed(DEFAULT_SPEED, true);
 
   if (distance <= DETECT_DIST) {
-    motion1.stop();
-    motion2.stop();
-    delay(SMALL_DELAY);
+    dodge_an_obstacle();
+  }
 
-    motion1.rotate_to_left();
-    motion2.rotate_to_left();
+  if (!ir[2]) {
     motion1.forward();
     motion2.forward();
-    delay(SMALL_DELAY);
+  } else if (!ir[1]) {
+    motion1.right();
+    motion2.right();
+  } else if (!ir[3]) {
+    motion1.left();
+    motion2.left();
+  }
+}
 
-    motion1.rotate_to_right();
-    motion2.rotate_to_right();
-    motion1.forward();
-    motion2.forward();
-    delay(SMALL_DELAY);
+void dodge_an_obstacle() {
+  motion1.stop();
+  motion2.stop();
+  delay(SMALL_DELAY);
 
-    motion1.rotate_to_right();
-    motion2.rotate_to_right();
-    motion1.forward();
-    motion2.forward();
-    delay(SMALL_DELAY);
+  motion1.rotate_to_left();
+  motion2.rotate_to_left();
+  motion1.forward();
+  motion2.forward();
+  delay(SMALL_DELAY);
 
-    motion1.rotate_to_left();
-    motion2.rotate_to_left();
-    motion1.forward();
-    motion2.forward();
-    delay(SMALL_DELAY);
-  }*/
+  motion1.rotate_to_right();
+  motion2.rotate_to_right();
+  motion1.forward();
+  motion2.forward();
+  delay(SMALL_DELAY);
+
+  motion1.rotate_to_right();
+  motion2.rotate_to_right();
+  motion1.forward();
+  motion2.forward();
+  delay(SMALL_DELAY);
+
+  motion1.rotate_to_left();
+  motion2.rotate_to_left();
+  motion1.forward();
+  motion2.forward();
+  delay(SMALL_DELAY);
 }
